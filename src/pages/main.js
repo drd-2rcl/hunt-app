5e5e9f7ae75e24220ec1d055238a5b10ba3ae01a
drd-2rcl <drd.rclrd@gmail.com>
2019-04-07 13:25:18 -0300

    Alter styles to text inside main page.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-
 import { View, Text } from 'react-native';
 
 export default class Main extends Component {
   static navigationOptions = {
-    title: 'JS LOVE'
+    title: 'JSLOVE'
   }
   render () {
     return (
-      <View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
         <Text>Página Main</Text>
       </View>
     )
