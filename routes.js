817cdf5ede6c909735aa40d000a1fe3ea681b6c3
drd-2rcl <drd.rclrd@gmail.com>
2019-04-07 18:52:19 -0300

    New route products

--- routes.js
+++ routes.js
@@ -1,17 +1,21 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import Main from './src/pages/main'
+import Product from './src/pages/product'
 
-const AppNavigator = createStackNavigator({
-  Main
-}, {
-  navigationOptions: {
-    headerStyle: {
-      backgroundColor: "#DA552F"
-    },
-    headerTintColor: "#ff0000"
+const AppNavigator = createStackNavigator(
+  {
+    Main,
+    Product
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#DA552F"
+      },
+      headerTintColor: "#ff0000"
+    }
   }
-}
 );
 
  const AppContainer =  createAppContainer(AppNavigator);
