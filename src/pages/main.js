c8fa84688cb039be780c76fc81127ab600949aa0
drd-2rcl <drd.rclrd@gmail.com>
2019-04-07 13:46:13 -0300

    Create method to call api init application.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api, { baseURL } from '../services/api';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: 'JSLOVE'
+  };
+
+  componentDidMount() {
+    this.loadProducts();
   }
+
+  loadProducts = async () => {
+    const response = await api.get(baseURL);
+
+    const { docs } = response.data;
+    console.log(docs);
+  };
+
   render () {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
