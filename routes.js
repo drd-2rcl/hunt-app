c42528e7de0bc16bca79b2367a7a522689a24364
drd-2rcl <drd.rclrd@gmail.com>
2019-04-07 13:24:03 -0300

    Set navigations options and header styles.

--- routes.js
+++ routes.js
@@ -4,9 +4,19 @@ import Main from './src/pages/main'
 
 const AppNavigator = createStackNavigator({
   Main
-});
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: "#DA552F"
+    },
+    headerTintColor: "#ff0000"
+  }
+}
+);
 
-const AppContainer =  createAppContainer(AppNavigator);
+ const AppContainer =  createAppContainer(AppNavigator);
 
-export default AppContainer;
+
+ export default AppContainer;
+
 
